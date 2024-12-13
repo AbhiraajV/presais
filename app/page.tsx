21f1e03f5034f0d6724240925178c19964ce6dff
AbhiraajV <abhiraajverma@gmail.com>
2024-12-13 13:38:53 +0530

    hollup

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,29 @@
 'use client'
 import { sendMessageToQueue } from "./actions/amq";
 import HomePageForm from "@/components/HomePageForm";
-import {
-  SignIn,
-} from '@clerk/nextjs'
 import { useEffect, useState } from "react";
 import { setGetUser } from "./actions/clerk.functions";
 import { User } from "@prisma/client";
+import { RedirectToSignIn } from "@clerk/nextjs";
 export default function Home() {
-  const [user,setUser] = useState<User | null>(null)
+  const [user,setUser] = useState<User | null | 'no user'>(null)
   useEffect(() => {
-    setGetUser().then(u=>setUser(u))
+    setGetUser().then(u=>{
+      if(u === null){
+         setUser('no user')
+         return;
+      }
+      setUser(u)
+    })
   }, [])
+  if(user === 'no user') return <RedirectToSignIn/>
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      {!user ? 
-        <SignIn/> :
-        <HomePageForm user={user} onSubmit={(v)=>sendMessageToQueue(JSON.stringify({prompt:JSON.stringify(v),userId:user.id}),user.id).then(()=>window.location.reload())}/>
-      }
+      {user ? <HomePageForm user={user} onSubmit={(v)=>sendMessageToQueue(JSON.stringify({prompt:JSON.stringify(v),userId:user.id}),user.id).then(()=>window.location.reload())}/>
+        :<span className="text-xl animate-ping font-extrabold">
+            🫦 hollup, 🫦 
+        </span>
+    }
     </div>
   );
 }
